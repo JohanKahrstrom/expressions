04eec78089079d5dbeeac2ed082fc0c7041428ed
Johan Kahrstrom <johan.kahrstrom@ocado.com>
2013-10-07 14:42:14 +0100

    Added test class

---
+++ src/test/scala/com/johankahrstrom/expressions/DeriveSuite.scala
@@ -0,0 +1,11 @@
+package com.johankahrstrom.expressions
+
+import org.junit.runner.RunWith
+
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+
+@RunWith(classOf[JUnitRunner])
+class DeriveSuite extends FunSuite {
+
+}
